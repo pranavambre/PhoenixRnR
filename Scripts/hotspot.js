fb6b029f35346017d706f652c72116331f0d0bb7
Pranav Ambre <pranavambre123@gmail.com>
2020-04-15 14:20:13 +1000

    Added prepare yourself house and hotspot

--- Scripts/hotspot.js
+++ Scripts/hotspot.js
@@ -1,22 +1,7 @@
-﻿
-function selectHotspot(e) {
-    const clickedHotspot = e.target.parentElement;
-    const container = clickedHotspot.parentElement;
-
-    // only include hotspots within same image to allow one open hotspot per image; change "container" to "document" to allow only one open hotspot for entire page:
-    const hotspots = container.querySelectorAll(".lg-hotspot");
-    hotspots.forEach(hotspot => {
-        if (hotspot === clickedHotspot) {
-            hotspot.classList.toggle("lg-hotspot--selected");
-        } else {
-            hotspot.classList.remove("lg-hotspot--selected");
-        }
-    });
-}
-
-(() => {
-    const buttons = document.querySelectorAll(".lg-hotspot__button");
-    buttons.forEach(button => {
-        button.addEventListener("click", selectHotspot);
-    });
-})();
+﻿$('.BornHS__Input').change(function () {
+    if ($(this).is(":checked")) {
+        $('.BornHS__Wrapper').addClass("BornHS__Blur");
+    } else {
+        $('.BornHS__Wrapper').removeClass("BornHS__Blur");
+    }
+});
