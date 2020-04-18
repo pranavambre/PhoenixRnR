55aede95e5b3fb970826a7334b79b9c0168a4b5c
Pranav Ambre <pranavambre123@gmail.com>
2020-04-19 00:47:13 +1000

    Added flamable items

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace PhoenixRnR
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"
-                     ));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
